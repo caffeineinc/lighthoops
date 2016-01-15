1e89a7d068b83f4d69ad17112a94eee4311f5ac3
Sam Minnee <sam@silverstripe.com>
2016-01-16 12:01:40 +1300

    Refactor out lib goods into GfxLib.ino.

--- Spinner.ino
+++ Spinner.ino
@@ -27,28 +27,3 @@ void fourColourWheel(CRGB *leds, int offset) {
   wrappedFill(leds, offset+58, 29, CRGB::Blue, LEDS_PER_HOOP);
   wrappedFill(leds, offset+87, 28, CRGB::Yellow, LEDS_PER_HOOP);
 }
-
-/**
- * Fill a region from start-length with colour, but wrap the fill within wrapSize
- * Calls 2 fill commands when needed by the underlying code.
- *
- * Breaks if length > wrapSize; don't do that.
- */
-void wrappedFill(CRGB *leds, int start, int length, CRGB color, int wrapSize) {
-  int segmentStart, segmentLength;
-  segmentStart = start % wrapSize;
-  if(segmentStart < 0) segmentStart += wrapSize;
-
-  // 1 segment
-  if(segmentStart + length < wrapSize) {
-    fill_solid(leds+segmentStart, length, color);
-
-  // 2 segments
-  } else {
-    segmentLength = wrapSize - segmentStart - 1;
-    fill_solid(leds + segmentStart, segmentLength, color);
-    fill_solid(leds, length - segmentLength, color);
-  }
-}
-  }
-}
